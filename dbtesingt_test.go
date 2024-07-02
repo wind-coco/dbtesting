0611ccf09353e9706810e99e4c41630f4a59797d
wind <shihaowind@163.com>
2024-07-02 13:41:12 +0800

    Initial commit

---
+++ dbtesingt_test.go
@@ -0,0 +1,22 @@
+package dbtesting
+
+import (
+	"github.com/wind-coco/dbtesting/config"
+	"os"
+	"testing"
+)
+
+func TestRunDBInDocker(t *testing.T) {
+
+}
+
+func TestMain(m *testing.M) {
+	os.Exit(RunDBInDocker(m, &config.Config{
+		Image:         "mysql:5.6",
+		User:          "root",
+		Password:      "123456",
+		DBName:        "test_db",
+		DB:            config.Mysql,
+		ContainerPort: "3306/tcp",
+	}))
+}
---
+++ env/builder.go
@@ -0,0 +1,32 @@
+package env
+
+import (
+	"testing-in-docker/config"
+	"testing-in-docker/env/mongo"
+	"testing-in-docker/env/mysql"
+)
+
+type Builder struct {
+	config.Builder
+}
+
+func NewBuilder(cfg *config.Config) *Builder {
+	var envBuilder config.Builder
+
+	switch cfg.DB {
+	case config.Mysql:
+		envBuilder = mysql.NewBuilder(cfg)
+	case config.Mongo:
+		envBuilder = mongo.NewBuilder(cfg)
+	}
+	return &Builder{
+		envBuilder,
+	}
+}
+
+func (b *Builder) BuildEnv() ([]string, error) {
+	return b.Builder.BuildEnv()
+}
+func (b *Builder) BuildURI(host, port string) (string, error) {
+	return b.Builder.BuildURI(host, port)
+}
